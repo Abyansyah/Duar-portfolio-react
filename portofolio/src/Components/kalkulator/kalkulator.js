4a2e39f4d6631363a657b82e7701eb0790f234e9
Ahmad Abyansyah <ahmadaby66@gmail.com>
2022-12-17 15:29:14 +0700

    navbar:fix

--- portofolio/src/Components/kalkulator/kalkulator.js
+++ portofolio/src/Components/kalkulator/kalkulator.js
@@ -4,11 +4,25 @@ import Kalkulator from './kalkulator/kalkulator';
 import Navbar from '../navbar/navbar';
 
 function Cal() {
-
+  const [darkMode, setDarkMode] = useState(true);
   return (
-    <div >
+    <div>
       <Navbar />
-      <Kalkulator/>
+      <div className={darkMode ? 'dark-mode' : 'light-mode'}>
+        <main className="container-page">
+          <Kalkulator />
+        </main>
+        <div className="container-check">
+          <span style={{ color: darkMode ? 'grey' : 'yellow' }}>☀︎</span>
+          <div className="switch-checkbox">
+            <label className="switch">
+              <input type="checkbox" onChange={() => setDarkMode(!darkMode)} />
+              <span className="slider round"> </span>
+            </label>
+          </div>
+          <span style={{ color: darkMode ? '#c96dfd' : 'grey' }}>☽</span>
+        </div>
+      </div>
     </div>
   );
 }
